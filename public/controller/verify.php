89b2c9cf42823bcbadca7f4a073e90781653b16c
Minh Hiếu <nguyenhieu3105@gmail.com>
2026-01-28 21:19:43 +0700

    update

--- public/controller/verify.php
+++ public/controller/verify.php
@@ -7,8 +7,10 @@ if(isset($_POST['admin_verify']) && $_POST['admin_verify']) {
     if(password_verify($input_verify,'$2y$10$3q4JO0xnkM1rQk/TnwLj/etaTfB7UWIPJElvqWqCQK0TsdUvDh99O')) {
         $_SESSION['btc'] = 'verify';
         toast_create('success','Xác thực thành công !');
+        route();
     }else {
         toast_create('failed','Xác thực thất bại !');
+        route('verify');
     }
 }
 
